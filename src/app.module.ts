22a021c9fe21238cae8476108ebdcce87055031b
Israel Ramos <arnulfo_israel@hotmail.com>
2024-04-20 18:08:48 -0600

    feat: project init

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      password: 'simform',
+      username: 'postgres',
+      entities: [],
+      database: 'pgWithNest',
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
